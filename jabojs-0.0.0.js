0c968992e5094dd40bcccd9c4fdff55ab0597ec6
Code Jabo <44330015+jabo-bernardo@users.noreply.github.com>
2019-05-22 20:00:03 +0800

    Update jabojs-0.0.0.js

--- jabojs-0.0.0.js
+++ jabojs-0.0.0.js
@@ -1,8 +1,11 @@
+// Jabo JS v0.0.0
+// Follow Jabo JS on twitter @jabo_js
+// For 
+
 // This library is developed by Code Jabo.
 // Follow Code Jabo on Twitter @jabo_bernardo
 
 
-// This is the canvas of the game underneath is the context of the canvas.
 var canvas;
 var ctx;
 
@@ -11,17 +14,26 @@ var FPS;
 var gameLoop = null;
 var renderLoop = null;
 
-window.onload = function() {
-	FPS = 120;
+var core;
 
-	gameLoop = setInterval(Update, 1000/FPS);
-	renderLoop = setInterval(Render, 1000/FPS);
+var gameObjects = [];
+
+window.onload = function() {
+
+	Initialize();
+
+	FPS = 60;
+
+	core = new Core();
+
+	changeScene(core);
 }
 
 // This function sets the values of the {canvas} and {ctx}
 function Initialize() {
 	canvas = document.getElementById("game");
 	ctx = canvas.getContext('2d');
+	canvas.onmousemove = mouseMove;
 }
 
 // Takes in two parameters x and y useful for coordinates.
@@ -46,18 +58,23 @@ var Sprite = function(src) {
 }
 
 // Game Object
-var GameObject = function(x, y, w, h) {
+var GameObject = function(x2, y2, w2, h2) {
 
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.h = h;
+	// identifiers
+	var tag = "GameObject";
+	gameObjects.push(this)
+	var id = gameObjects.length - 1;
+
+	this.x = x2;
+	this.y = y2;
+	this.w = w2;
+	this.h = h2;
 
 	var visible = true;
 	// Image of the object;
 	var texture;
-	var tag = "GameObject";
 
+	// Methods
 	this.Move = function(xMove, yMove) {
 		this.x += xMove;
 		this.y += yMove;
@@ -73,14 +90,22 @@ var GameObject = function(x, y, w, h) {
 		if(!visible)
 			return;
 		if(texture == null)
-			ctx.fillRect(x, y, w, h);
+			ctx.fillRect(this.x, this.y, this.w, this.h);
 		else {
 			try {
-				ctx.drawImage(texture, x, y, w, h);
+				ctx.drawImage(texture, this.x, this.y, this.w, this.h);
 			} catch(err) {
-				console.log("Failed to load texture for " + tag + "");
+				Debug("Failed to load texture for " + tag + "(" + id + "), You can try the ff.");
+				Debug("* use Sprite() see documentation[]");
+				Debug("* maybe the file is missing, you can try double checking your directory")
+
 			}
 		}
+	}
+
+	// Setters
+	this.setTexture = function(path) {
+		texture = path;
 	}
 
 	this.setVisibility = function(vis) {
@@ -92,14 +117,50 @@ var GameObject = function(x, y, w, h) {
 			visible = true;
 	}
 
-	this.setTexture = function(path) {
-		texture = path;
-	}
-
 	this.setTag = function(name) {
 		tag = name;
 	}
 
+	// Getters
+	this.getID = function() {
+		return id;
+	}
+
+}
+
+
+
+// Scene Manager
+function changeScene(scn) {
+
+	clearInterval(gameLoop);
+	clearInterval(renderLoop);
+
+	scn.Init();
+	gameLoop = setInterval(scn.Update, 1000/FPS);
+	renderLoop = setInterval(scn.Render, 1000/FPS);
+
+}
+
+// Inputs
+var Input = function() {
+
+	this.mouseX = 0;
+	this.mouseY = 0;
+
+}
+
+// Event Handlers
+function mouseMove(evt) {
+
+	Input.mouseX = evt.clientX;
+	Input.mouseY = evt.clientY;
+
+}
+
+// Misc
+function Debug(string) {
+	console.log(string);
 }
 
 // useful for clearing the canvas
@@ -112,6 +173,33 @@ function setMaxFps(fps) {
 	FPS = fps;
 }
 
-function importScene(path) {
-	document.write();
+function checkCollide(obj1, obj2) {
+
+	if((obj1.x + obj1.w) >= obj2.x && 
+		obj1.x <= (obj2.x + obj2.w)) {
+
+		if((obj1.y + obj1.h) >= obj2.y && 
+		obj1.y <= (obj2.y + obj2.h)) {
+
+			return true;
+
+		}
+
+	}
+	return false;
+}
+
+function getDistance(obj1, obj2) {
+
+	return Math.sqrt((obj2.x - obj1.x) * (obj2.x - obj1.x) + (obj2.y - obj1.y) * (obj2.y - obj1.y));
+
+}
+
+function Random(min, max) {
+
+	var num = Math.floor(Math.random() * max);
+	while(num < min)
+		num = Math.floor(Math.random() * max);
+	return num;
+
 }
